6103e621e93a092f737ab0f5a74578447fd4bbcc
Abhay  Tarone <abhaytarone78@gmail.com>
2026-01-31 13:40:36 +0530

    Refactor Jenkins pipeline stages and improve syntax

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,40 +1,48 @@
 pipeline {
     agent { label 'slave' }
+
+    tools {
+        maven 'maven3'
+        jdk 'java17'
+    }
+
     stages {
-        stage('git-pull-stage') {
+
+        stage('Checkout') {
             steps {
                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
             }
         }
-         stage('build-stage') {
+
+        stage('Build') {
             steps {
-                sh '/opt/maven/bin/mvn clean package'
-            }
-        }
-         stage('test-stage') {
-            steps {
-                withSonarQubeEnv(installationName: 'sonar',credentialsId: 'sona1r-cred') {
-                      sh '/opt/maven/bin/mvn sonar:sonar'
-                  }
-                // sh '''/opt/maven/bin/mvn sonar:sonar \\
-                //      -Dsonar.projectKey=studentapp \\
-                //      -Dsonar.host.url=http://13.53.200.180:9000 \\
-                //      -Dsonar.login=dfb451556e35114bf31b5798423e18c16fe839f2'''
-  
-            }
-        }
-         stage('Quality_Gate') {
-            steps {
-                timeout(3) {
-   
-            }
-                waitForQualityGate true
+                sh 'mvn clean package'
             }
         }
 
-         stage('deploy-stage') {
+        stage('SonarQube Analysis') {
             steps {
-                echo 'code deploy sucessfully'
+                withSonarQubeEnv('sonar') {
+                    sh '''
+                    mvn clean verify sonar:sonar \
+                      -Dsonar.projectKey=student-ui-app \
+                      -Dsonar.projectName=student-ui-app
+                    '''
+                }
+            }
+        }
+
+        stage('Quality Gate') {
+            steps {
+                timeout(time: 10, unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo "Application deployed successfully"
             }
         }
     }
