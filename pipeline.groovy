e26bfb62e6d070ad5a0d214f240ef38c19948aa6
Abhay  Tarone <abhaytarone78@gmail.com>
2026-01-31 13:46:16 +0530

    Update Maven command path in Build stagekk

--- pipeline.groovy
+++ pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
 
         stage('Build') {
             steps {
-                sh 'mvn clean package'
+                sh '/opt/maven/bin/mvn clean package'
             }
         }
 
