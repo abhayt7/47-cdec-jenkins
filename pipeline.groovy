4e4bbc88fb91f313fbf6705c037c14978b3ecc7a
Abhay  Tarone <abhaytarone78@gmail.com>
2026-01-31 14:51:22 +0530

    Disable Artifactory stage in pipeline
    Comment out Artifactory stage in the pipeline.

--- pipeline.groovy
+++ pipeline.groovy
@@ -35,11 +35,11 @@ pipeline {
             }
         }
 
-         stage('Artifatory-stage') {
-            steps {
-               sh 'aws s3 cp  target/studentapp-2.2-SNAPSHOT.war  s3://anurag-1234/'
-            }
-        }
+         //stage('Artifatory-stage') {
+            //steps {
+              // sh 'aws s3 cp  target/studentapp-2.2-SNAPSHOT.war  s3://anurag-1234/'
+           // }
+       // }
 
         stage('Deploy') {
             steps {
